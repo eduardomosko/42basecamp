a52ce88e6451d913c00e75bb07a92490ce4d5703
emendes- <emendes-@student.42sp.org.br>
2021-03-31 21:03:14 +0000

    ex 06

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/31 20:49:35 by emendes-          #+#    #+#             */
+/*   Updated: 2021/03/31 21:02:13 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_comb2(void)
+{
+	int counter;
+	char c;
+	char sep[2];
+
+	counter = 0;
+	sep[0] = ',';
+	sep[1] = ' ';
+	while (counter < 10000)
+	{
+		c = counter / 1000 + '0';
+		write(1, &c, 1);
+		c = counter / 100 % 10 + '0';
+		write(1, &c, 1);
+		write(1, &sep[1], 1);
+		c = counter / 10 % 10 + '0';
+		write(1, &c, 1);
+		c = counter % 10 + '0';
+		write(1, &c, 1);
+		write(1, &sep, 2);
+		++counter;
+	}
+}
