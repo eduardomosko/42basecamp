18f66679dc361615d18072ceb97dfda7c6b3b0d1
emendes- <emendes-@student.42sp.org.br>
2021-03-31 00:14:21 +0000

    ex07 certo

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,2 @@
-sed /etc/passwd -e "/^\s*#/d" | awk "BEGIN { C = 0 } { if (C % 2) print $1; C = C + 1}" | cut -d ":" -f1 | rev | sort -r | sed -n "$FT_LINE1, $FT_LINE2 p" | paste -sd, | tr -d "\n"
+#!/bin/sh
+sed /etc/passwd -e "/^\s*#/d" -ne "2~2p" | cut -d ":" -f1 | rev | sort -r | sed -n -e "$FT_LINE1, $FT_LINE2 p" | paste -sd, | sed -e "s/,/, /g" -e "s/$/./" | tr -d "\n"
