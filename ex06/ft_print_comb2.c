c32f79058d5eea5be7619c22c1232d848ffb1a05
emendes- <emendes-@student.42sp.org.br>
2021-04-01 07:51:18 +0000

    ex 06 certo

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 20:49:35 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/01 06:00:39 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/01 07:50:51 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,29 @@
 
 void	ft_print_comb2(void)
 {
-	int		counter;
+	int		counter1;
+	int		counter2;
 	char	c;
-	char	sep[2];
 
-	counter = 0;
-	sep[0] = ',';
-	sep[1] = ' ';
-	while (counter < 10000)
+	write(1, "00 01", 5);
+	counter1 = 0;
+	counter2 = 2;
+	while (counter1 < 100 && counter2 < 100)
 	{
-		c = counter / 1000 + '0';
+		write(1, ", ", 2);
+		c = counter1 / 10 + '0';
 		write(1, &c, 1);
-		c = counter / 100 % 10 + '0';
+		c = counter1 % 10 + '0';
 		write(1, &c, 1);
-		write(1, &sep[1], 1);
-		c = counter / 10 % 10 + '0';
+		write(1, " ", 1);
+		c = counter2 / 10 + '0';
 		write(1, &c, 1);
-		c = counter % 10 + '0';
+		c = counter2 % 10 + '0';
 		write(1, &c, 1);
-		write(1, &sep, 2);
-		++counter;
+		if (counter2 == 100)
+		{
+			counter2 = ++counter1;
+		}
+		++counter2;
 	}
 }
