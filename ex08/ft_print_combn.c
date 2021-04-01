64fd0b0b355deec44952d97a25f9550a6fb3b918
emendes- <emendes-@student.42sp.org.br>
2021-04-01 07:26:45 +0000

    Ex 08 funciona e a norminette aceita

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 00:23:17 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/01 07:10:48 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/01 07:25:51 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,24 @@ void	ft_print_combn(int n)
 {
 	char	num[9];
 	char	max[9];
-	char	separator[2];
 	int		c;
 
-	c = n - 1;
-	while (c >= 0)
+	c = -1;
+	while (++c < n)
 	{
 		max[c] = '9' - (n - 1) + c;
 		num[c] = '0' + c;
-		--c;
 	}
-	c = n - 1;
 	write(1, num, n);
 	while (c >= 0)
 	{
 		if (num[c] == max[c])
 			--c;
-		else if (c != n - 1)
-		{
-			++c;
+		else if (++c != n)
 			num[c] = num[c - 1];
-		}
-		++num[c];
-		if (c == n - 1 || num[c] == max[c])
+		else
+			--c;
+		if (++num[c] == max[c] || c == n - 1)
 		{
 			write(1, ", ", 2);
 			write(1, num, n);
