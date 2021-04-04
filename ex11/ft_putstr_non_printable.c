70b852f0b0ed90523520b6efd14e5d9e472c2496
emendes- <emendes-@student.42sp.org.br>
2021-04-04 15:32:48 +0000

    ex11 certo

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 03:02:09 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/04 02:51:16 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/04 15:23:38 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ const char	g_lookup[] = "0123456789abcdef";
 
 void	ft_putstr_non_printable(char *str)
 {
-	char	buf[4];
-	int		offset;
+	char	buf[3];
 
 	buf[0] = '\\';
 	while (*str != '\0')
@@ -26,15 +25,9 @@ void	ft_putstr_non_printable(char *str)
 			write(1, str, 1);
 		else
 		{
-			offset = 0;
-			if (*str < 0)
-			{
-				offset = 1;
-				buf[1] = '-';
-			}
-			buf[1 + offset] = g_lookup[((*str / 16) % 16) * (offset ? -1 : 1)];
-			buf[2 + offset] = g_lookup[*str % 16 * (offset ? -1 : 1)];
-			write(1, buf, (3 + offset));
+			buf[1] = g_lookup[((unsigned char)*str) / 16];
+			buf[2] = g_lookup[((unsigned char)*str) % 16];
+			write(1, buf, 3);
 		}
 		++str;
 	}
