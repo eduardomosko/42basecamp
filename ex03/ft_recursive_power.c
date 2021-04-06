179e992fb685065cda1d592b069bc0f262a553f1
emendes- <emendes-@student.42sp.org.br>
2021-04-06 14:11:39 +0000

    exercicios power

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/06 14:05:00 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/06 14:09:53 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (ft_recursive_power(nb, power - 1) * nb);
+}
