25695508a7f5ad9b51643ddbc090815d434da977
emendes- <emendes-@student.42sp.org.br>
2021-04-07 02:04:14 +0000

    ex07 norma

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 15:17:21 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/06 15:25:49 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/06 17:28:26 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static int	ft_is_prime(int nb)
 	return (nb > 1);
 }
 
-int	ft_find_next_prime(int nb)
+int			ft_find_next_prime(int nb)
 {
 	while (!ft_is_prime(nb))
 		++nb;
