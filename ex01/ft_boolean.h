6ea27befbbe575d7930b26010e7ab4ea39be2b2e
emendes- <emendes-@student.42sp.org.br>
2021-04-11 14:36:54 +0000

    ex02

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/11 14:31:03 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/11 14:35:30 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# define EVEN(x) ((x) % 2 == 0)
+# define TRUE 1
+# define FALSE 1
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
