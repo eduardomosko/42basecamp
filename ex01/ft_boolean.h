225f0ba56640a88362c357b5e77c556144c161d4
emendes- <emendes-@student.42sp.org.br>
2021-04-12 13:19:11 +0000

    ex01 funcionando

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,19 +6,20 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 14:31:03 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/11 14:35:30 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/12 13:18:24 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
+# include <unistd.h>
+
 # define EVEN(x) ((x) % 2 == 0)
 # define TRUE 1
-# define FALSE 1
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
-# define SUCCESS 0
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 
 typedef int	t_bool;
