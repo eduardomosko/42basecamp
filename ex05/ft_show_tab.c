b600dade60e178fe05194b4588fa8a988e24f4f0
emendes- <emendes-@student.42sp.org.br>
2021-04-12 13:12:46 +0000

    ex05

---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/11 21:15:24 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/12 13:11:58 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct	s_stock_str
+{
+	int size;
+	char* str;
+	char* copy;
+}				t_stock_str;
+
+void	ft_putnbr(int nb)
+{
+	char			c;
+	unsigned int	div;
+	unsigned int	num;
+
+	if (nb < 0)
+		write(1, "-", 1);
+	num = nb < 0 ? (-1) * nb : nb;
+	div = 1;
+	while (nb / 10)
+	{
+		nb = nb / 10;
+		div *= 10;
+	}
+	while (div)
+	{
+		c = '0' + num / div;
+		write(1, &c, 1);
+		num = num % div;
+		div = div / 10;
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	if (par == NULL)
+		return ;
+	while (par->str != 0)
+	{
+		write(1, par->str, par->size);
+		write(1, "\n", 1);
+		ft_putnbr(par->size);
+		write(1, "\n", 1);
+		write(1, par->copy, par->size);
+		write(1, "\n", 1);
+		++par;
+	}
+}
