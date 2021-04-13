9ed95d06fa092212e4adfc33e6ea7a19a008e443
emendes- <emendes-@student.42sp.org.br>
2021-04-13 04:47:14 +0000

    created Makefile and finished errors.c

--- ex01/errors.c
+++ ex01/errors.c
@@ -6,9 +6,13 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 04:30:57 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/13 04:39:40 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/13 04:46:24 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 /*
 ** Calculates the length of a given string
@@ -16,7 +20,12 @@
 
 unsigned int	ft_strlen(const char *str)
 {
-	return (1);
+	unsigned int n;
+
+	n = 0;
+	while (str[n] != '\0')
+		++n;
+	return (n);
 }
 
 /*
@@ -33,4 +42,5 @@ void			show_error(const char *progname, const char *filename)
 	write(1, ": ", 2);
 	strerr = strerror(errno);
 	write(1, strerr, ft_strlen(strerr));
+	write(1, "\n ", 1);
 }
