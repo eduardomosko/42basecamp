8d6bc81abffa7a9a10ce47cdad7588aa0c412659
emendes- <emendes-@student.42sp.org.br>
2021-04-14 21:10:08 +0000

    ex11

---
+++ ex11/ft_list_find.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 20:49:49 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/14 20:59:12 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	while (begin_list != NULL)
+	{
+		if (cmp(begin_list->data, data_ref) == 0)
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return (NULL);
+}
