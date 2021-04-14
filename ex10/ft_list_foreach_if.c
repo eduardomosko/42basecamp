12424d399b6ade8c36f62d8cc4c1edaa78aa43f7
emendes- <emendes-@student.42sp.org.br>
2021-04-14 20:48:21 +0000

    ex10

---
+++ ex10/ft_list_foreach_if.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach_if.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 20:44:05 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/14 20:48:13 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach_if(t_list *begin_list, void (*f)(void*), void *data_ref, int (*cmp)())
+{
+	while (begin_list != NULL)
+	{
+		if (cmp(begin_list->data, data_ref) == 0)
+			f(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
