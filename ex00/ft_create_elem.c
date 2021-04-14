bd1203cd38efbfcc31e506f6eba3900f2301fcc6
emendes- <emendes-@student.42sp.org.br>
2021-04-14 19:31:57 +0000

    ex00

---
+++ ex00/ft_create_elem.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 19:15:32 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/14 19:17:44 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list		*ft_create_elem(void *data)
+{
+	t_list *ret;
+
+	ret = malloc(sizeof(t_list));
+	if (ret != NULL)
+	{
+		ret->data = data;
+		ret->next = NULL;
+	}
+	return (ret);
+}
