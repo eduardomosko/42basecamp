3aad2cad341c8d69e9d2a4289d6d47c6293ebe50
emendes- <emendes-@student.42sp.org.br>
2021-04-14 19:43:00 +0000

    ex04

---
+++ ex04/ft_list_push_back.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 19:41:39 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/14 19:42:48 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_list_push_back(t_list **begin_list, void *data)
+{
+	if (begin_list == NULL)
+		return (NULL);
+	while (begin_list->next != NULL)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
