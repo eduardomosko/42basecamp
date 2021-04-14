a1dd5e0a8673e03119e2af205fe11650bf18717b
emendes- <emendes-@student.42sp.org.br>
2021-04-14 19:42:53 +0000

    ex03

---
+++ ex03/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 19:39:31 by emendes-          #+#    #+#             */
+/*   Updated: 2021/04/14 19:40:44 by emendes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	if (begin_list == NULL)
+		return (NULL);
+	while (begin_list->next != NULL)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
