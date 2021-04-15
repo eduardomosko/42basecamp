d3d95d25e318c0a02d936a6dfdbf286cf3bc5788
emendes- <emendes-@student.42sp.org.br>
2021-04-15 15:28:25 +0000

    ex03 correcto

--- ex03/ft_count_if.c
+++ ex03/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 17:56:11 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/14 19:19:32 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/15 15:27:29 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_count_if(char **tab, int length, int (*f)(char*))
 	int n;
 	int i;
 
-	if (tab == NULL)
+	if (tab == 0)
 		return (0);
 	n = 0;
 	i = 0;
