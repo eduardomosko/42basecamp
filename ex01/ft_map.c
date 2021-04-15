291b2d507fe30a21fc7dea8c0c5ca10d421b88e2
emendes- <emendes-@student.42sp.org.br>
2021-04-15 15:15:54 +0000

    ex01 correto

--- ex01/ft_map.c
+++ ex01/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 14:57:17 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/14 19:18:24 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/15 15:14:27 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 int	*ft_map(int *tab, int length, int (*f)(int))
 {
 	int *ret;
+	int i;
 
-	if ((ret = malloc(tab * sizeof(int))) == NULL)
+	if ((ret = malloc(length * sizeof(int))) == NULL)
 		return (NULL);
-	while (i < n)
+	i = 0;
+	while (i < length)
 	{
 		ret[i] = f(tab[i]);
+		++i;
 	}
 	return (ret);
 }
