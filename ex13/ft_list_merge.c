c9ae2c640d22e5e268891d82fc2186e13acdca4c
emendes- <emendes-@student.42sp.org.br>
2021-04-15 23:39:35 +0000

    ex13

--- ex13/ft_list_merge.c
+++ ex13/ft_list_merge.c
@@ -6,10 +6,17 @@
 /*   By: emendes- <emendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 21:41:08 by emendes-          #+#    #+#             */
-/*   Updated: 2021/04/15 21:41:42 by emendes-         ###   ########.fr       */
+/*   Updated: 2021/04/15 23:16:21 by emendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_merge(t_list **begin_list1, t_list *begin_list2);
+void	ft_list_merge(t_list **begin_list1, t_list *begin_list2)
+{
+	if (begin_list1 == 0)
+		return ;
+	while (*begin_list1 != NULL)
+		begin_list = &((*begin_list1)->next);
+	*begin_list1 = begin_list2;
+}
